910636e824ec3a8aac5a60210a9032c63ac13208
Otavio Augusto <tavioalves@gmail.com>
2025-05-22 14:21:58 -0300

    feat: Create getUploads route and register into server

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -9,6 +9,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { env } from "../env";
+import { getUploadsRoute } from "./routes/get-uploads";
 import { uploadImageRoute } from "./routes/upload-image";
 import { transformSwaggerSchema } from "./transform-swagger-schema";
 
@@ -47,6 +48,7 @@ server.register(fastifySwaggerUi, {
 });
 
 server.register(uploadImageRoute);
+server.register(getUploadsRoute);
 
 console.log(env.DATABASE_URL);
 
